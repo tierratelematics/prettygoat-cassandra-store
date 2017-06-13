81ae35e452dad75928292553e14c506abe2fb2eb
Mattia Franchetto <mt.franchetto@gmail.com>
2017-06-13 12:28:40 +0200

    Add inconclusive cassandra deserializer spec

---
+++ test/CassandraDeserializerSpec.ts
@@ -0,0 +1,8 @@
+import "reflect-metadata";
+import expect = require("expect.js");
+
+describe("Given a cassandra deserializer", () => {
+    context("when an event is read", () => {
+        it("should be parsed correctly");
+    });
+});
