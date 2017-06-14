dad2fc82f567c8a73eb8eeb2ed032ec987953575
Mattia Franchetto <mt.franchetto@gmail.com>
2017-06-14 17:01:32 +0200

    Implement time partitioner

--- scripts/TimePartitioner.ts
+++ scripts/TimePartitioner.ts
@@ -2,9 +2,9 @@ import * as moment from "moment";
 import {injectable, inject} from "inversify";
 import {IDateRetriever} from "prettygoat";
 
-export type BucketForManifest = {
-    entityBucket: string;
-    manifestBuckets: string[];
+export type Bucket = {
+    entity: string;
+    manifest: string;
 }
 
 @injectable()
@@ -14,12 +14,16 @@ export class TimePartitioner {
 
     }
 
-    bucketsFrom(date: Date): BucketForManifest[] {
-        let buckets: string[] = [];
+    bucketsFrom(date: Date): Bucket[] {
+        let buckets: Bucket[] = [];
 
         while (this.dateRetriever.getDate() >= date) {
-            buckets.push(moment(date).format("YYYYMMDD"));
-            date = moment(date).add(1, "days").hour(0).second(0).minute(0).toDate();
+            let momentDate = moment.utc(date).second(0).minute(0);
+            buckets.push({
+                entity: momentDate.format("YYYY0000T000000") + "Z",
+                manifest: momentDate.format("YYYYMMDDTHHmmss") + "Z"
+            });
+            date = momentDate.add(1, "hours").toDate();
         }
         return buckets;
     }
--- test/TimePartitionerSpec.ts
+++ test/TimePartitionerSpec.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import expect = require("expect.js");
 import {TimePartitioner} from "../scripts/TimePartitioner";
 import {IMock, Mock} from "typemoq";
@@ -16,13 +17,22 @@ describe("TimePartitioner, given a date", () => {
     context("when the list of the time buckets from now is needed", () => {
         context("and it's the same year", () => {
             beforeEach(() => {
-                dateRetriever.setup(d => d.getDate()).returns(() => new Date("2016-06-04T08:49:22.209Z"));
+                dateRetriever.setup(d => d.getDate()).returns(() => new Date("2016-06-06T08:49:22.209Z"));
             });
 
             it("should build it", () => {
                 expect(subject.bucketsFrom(new Date("2016-06-06T05:48:22.206Z"))).to.eql([{
-                    entityBucket: "20160000T000000Z",
-                    manifestBuckets: ["20160606T050000Z", "20160606T060000Z", "20160606T070000Z", "20160606T080000Z"]
+                    entity: "20160000T000000Z",
+                    manifest: "20160606T050000Z"
+                }, {
+                    entity: "20160000T000000Z",
+                    manifest: "20160606T060000Z"
+                }, {
+                    entity: "20160000T000000Z",
+                    manifest: "20160606T070000Z"
+                }, {
+                    entity: "20160000T000000Z",
+                    manifest: "20160606T080000Z"
                 }]);
             });
         });
@@ -32,13 +42,16 @@ describe("TimePartitioner, given a date", () => {
                 dateRetriever.setup(d => d.getDate()).returns(() => new Date("2017-01-01T01:48:22.206Z"));
             });
 
-            it("should build another entity_bucket", () => {
+            it("should build another entity bucket", () => {
                 expect(subject.bucketsFrom(new Date("2016-12-31T23:49:22.209Z"))).to.eql([{
-                    entityBucket: "20160000T000000Z",
-                    manifestBuckets: ["20161231T230000Z"]
+                    entity: "20160000T000000Z",
+                    manifest: "20161231T230000Z"
                 }, {
-                    entityBucket: "20170000T000000Z",
-                    manifestBuckets: ["20170101T000000Z", "20170101T010000Z"]
+                    entity: "20170000T000000Z",
+                    manifest: "20170101T000000Z"
+                }, {
+                    entity: "20170000T000000Z",
+                    manifest: "20170101T010000Z"
                 }]);
             });
         });
