98d098e9cd6799824563c35d018dff60ffc2ba61
Mattia Franchetto <mt.franchetto@gmail.com>
2017-08-04 15:41:01 +0200

    Revert "Remove new polling logic from poll to push"
    This reverts commit f3371280c059236331cff31df3e9d98e9dbf3c66.

--- scripts/stream/PollToPushStreamFactory.ts
+++ scripts/stream/PollToPushStreamFactory.ts
@@ -4,27 +4,39 @@ import {Observable} from "rx";
 import {DefaultPollToPushConfig, IPollToPushConfig} from "../config/PollToPushConfig";
 
 const REALTIME = "__prettygoat_internal_realtime";
+const EMPTY_POLLING = "__prettygoat_internal_empty_poll";
 
 @injectable()
 class PollToPushStreamFactory implements IStreamFactory {
 
     constructor(@inject("StreamFactory") private streamFactory: IStreamFactory,
-                @inject("IPollToPushConfig") @optional() private config: IPollToPushConfig = new DefaultPollToPushConfig()) {
+                @inject("IPollToPushConfig") @optional() private config: IPollToPushConfig = new DefaultPollToPushConfig(),
+                @inject("IDateRetriever") private dateRetriever: IDateRetriever) {
 
     }
 
     from(lastEvent: Date, completions?: Observable<string>, definition?: IWhen<any>): Observable<Event> {
+        let pollTime = this.dateRetriever.getDate();
         return this.streamFactory
             .from(lastEvent, completions, definition)
             .concat(Observable.just(this.eventWithManifest(REALTIME)))
             .concat(Observable
                 .interval(this.config.interval)
-                .flatMapWithMaxConcurrent(1, _ => this.streamFactory.from(lastEvent, completions, definition))
+                .do(() => pollTime = this.dateRetriever.getDate())
+                .flatMapWithMaxConcurrent(1, _ => this.streamFactory
+                    .from(lastEvent, completions, definition)
+                    .defaultIfEmpty(this.eventWithManifest(EMPTY_POLLING)))
             )
             .do(event => {
                 if (event.timestamp)
                     lastEvent = event.timestamp;
-            });
+                // Move forward the poll time if some buckets can be skipped in the next iteration (in order to avoid stressing cassandra)
+                // since I'm in realtime (and no more events are produced in the past) or an empty poll cycle
+                // has been done
+                if ((event.type === REALTIME || event.type === EMPTY_POLLING) && pollTime > lastEvent)
+                    lastEvent = pollTime;
+            })
+            .filter(event => event.type !== EMPTY_POLLING);
     }
 
     private eventWithManifest(manifest: string): Event {
